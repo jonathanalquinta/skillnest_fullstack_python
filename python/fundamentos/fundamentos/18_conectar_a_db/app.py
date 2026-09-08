e98d83658ce6bcc2fc6e5648ba4fa02d6100c7e4
jonathanalquinta <jonathanalquinta@liceovvh.cl>
2026-09-08 12:15:49 -0300

    act
    act

---
+++ python/fundamentos/fundamentos/18_conectar_a_db/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+from mascota import Mascota
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    mascotas = Mascota.get_all()
+    return render_template("index.html", mascotas=mascotas)
+
+if __name__ == "__main__":
+    app.run(debug=True)
